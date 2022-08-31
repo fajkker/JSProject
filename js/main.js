5b3ab8e9bbdc1d6402a1ca4db6ada518bad13581
fajkker <fedeparadelo@gmail.com>
2022-08-31 14:37:31 -0300

    New page

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ $('#returnBtn').click(function() {
     location.href="index.html";
 });
 
+$('#classBtn').click(function() {
+    location.href="class.html";
+});
+
 function getNumRand(min, max) {       
     return Math.round(Math.random()*(max-min)+parseInt(min));
 }
 
 var clase = ["Mago", "Guerrero", "Bardo", "Paladin"];
 clase.push("Arquero")
-document.getElementById("titulo").innerHTML = "Tienes " + clase.length + " clases para elegir";
+document.getElementById("clase").innerHTML = "Tienes " + clase.length + " clases para elegir";

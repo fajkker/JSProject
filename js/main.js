169fb46d8403692d2c8d4735101aee9461869668
fajkker <fedeparadelo@gmail.com>
2022-09-03 19:45:34 -0300

    Mostrar y ocultar

--- js/main.js
+++ js/main.js
@@ -10,25 +10,77 @@ $('#classBtn').click(function() {
     location.href="class.html";
 });
 
-$('#mage').click(function() {
-    location.href="#";
-});
 
-$('#warrior').click(function() {
-    location.href="#";
-});
+function mostrarM(input) {
+    var mage=document.getElementById("mage")
+    if(input.value=="Mago") {
+        mage.style.display = "block";
+        input.value="Ocultar"
+    } else {
+        mage.style.display = "none"
+        input.value="Mago"
+    }
 
-$('#paladin').click(function() {
-    location.href="#";
-});
+    //document.getElementById("mage").style.display = 'block';
+    //alert("Magos usan " + clase.arma)
 
-$('#bardo').click(function() {
-    location.href="#";
-});
+};
 
-$('#arquero').click(function() {
-    location.href="#";
-});
+function mostrarW(input) {
+    var warrior=document.getElementById("warrior")
+    if(input.value=="Warrior") {
+        warrior.style.display = "block";
+        input.value="Ocultar"
+    } else {
+        warrior.style.display = "none"
+        input.value="Warrior"
+    }
+
+    //function warrior() {
+    //    document.getElementById("warrior").style.display = 'block';
+};
+
+function mostrarP(input) {
+    var paladin=document.getElementById("paladin")
+    if(input.value=="Paladin") {
+        paladin.style.display = "block";
+        input.value="Ocultar"
+    } else {
+        paladin.style.display = "none"
+        input.value="Paladin"
+    }
+
+    //function paladin() {
+    //     document.getElementById("paladin").style.display = 'block';
+};
+
+function mostrarB(input) {
+    var bardo=document.getElementById("bardo")
+    if(input.value=="Bardo") {
+        bardo.style.display = "block";
+        input.value="Ocultar"
+    } else {
+        bardo.style.display = "none"
+        input.value="Bardo"
+    }
+
+    //function bardo() {
+    //     document.getElementById("bardo").style.display = 'block';
+};
+
+function mostrarA(input) {
+    var arquero=document.getElementById("arquero")
+    if(input.value=="Arquero") {
+        arquero.style.display = "block";
+        input.value="Ocultar"
+    } else {
+        arquero.style.display = "none"
+        input.value="Arquero"
+    }
+
+    //function arquero() {
+    //     document.getElementById("arquero").style.display = 'block';
+};
 
 function getNumRand(min, max) {       
     return Math.round(Math.random()*(max-min)+parseInt(min));
@@ -56,3 +108,8 @@ console.log(nombres)
 
 var armas = clase.map((el => el.arma))
 console.log(armas)
+
+//function mostrar() {
+//    document.getElementById("mage").style.display = 'block';
+//    alert("Magos usan " + clase(nombre("Mago")).arma)
+//};
